f015b30b9419a2333feee43d2ad622f96f9a964d
grandfrog0 <maksspecial32@gmail.com>
2026-01-22 11:48:51 +0500

    autoconnect added

--- Assets/Scripts/Net/NetworkUI.cs
+++ Assets/Scripts/Net/NetworkUI.cs
@@ -1,9 +1,54 @@
+using System;
+using System.Collections;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Experimental.GlobalIllumination;
 
 public class NetworkUI : MonoBehaviour
 {
+    public UnityEvent OnConnected = new();
+    public UnityEvent OnStopped = new();
+
+    private Coroutine _searchRoutine;
+    public void StartSearch()
+    {
+        if (NetworkManager.Singleton.IsServer || NetworkManager.Singleton.IsClient)
+            throw new Exception(NetworkManager.Singleton.IsServer + " || " + NetworkManager.Singleton.IsClient);
+
+        _searchRoutine = StartCoroutine(SearchRoutine());
+    }
+    public void StopSearch()
+    {
+        if (_searchRoutine != null)
+        {
+            StopCoroutine(_searchRoutine);
+            _searchRoutine = null;
+        }
+        Stop();
+    }
+
+    private IEnumerator SearchRoutine()
+    {
+        NetworkManager.Singleton.StartClient();
+
+        yield return new WaitForSeconds(5f);
+
+        if (!NetworkManager.Singleton.IsConnectedClient)
+        {
+            Debug.Log("Сервер не найден, запускаю хост...");
+            NetworkManager.Singleton.Shutdown();
+            yield return new WaitForSeconds(1f);
+            StartHost();
+        }
+        else
+        {
+            Debug.Log("Client started!");
+            OnConnected.Invoke();
+        }
+    }
+
     public void StartHost()
     {
         if (NetworkManager.Singleton.IsServer || NetworkManager.Singleton.IsClient)
@@ -11,6 +56,7 @@ public class NetworkUI : MonoBehaviour
 
         NetworkManager.Singleton.StartHost();
         Debug.Log("Host started!");
+        OnConnected.Invoke();
     }
 
     public void StartClient()
@@ -20,6 +66,7 @@ public class NetworkUI : MonoBehaviour
 
         NetworkManager.Singleton.StartClient();
         Debug.Log("Client started!");
+        OnConnected.Invoke();
     }
 
     public void Stop()
@@ -28,6 +75,7 @@ public class NetworkUI : MonoBehaviour
         {
             NetworkManager.Singleton.Shutdown();
             Debug.Log("Stopped.");
+            OnStopped.Invoke();
         }
     }
 }
